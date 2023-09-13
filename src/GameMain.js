b7fcf95a66ae91a3cbb82bdec5fa403d0964837a
dfriedman10 <friedmandavid10@gmail.com>
2023-09-13 11:18:41 -0400

    option panel sizing updated

--- src/GameMain.js
+++ src/GameMain.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PokeGuesser from "./PokeGuesser";
 import WelcomeScreen from "./WelcomeScreen";
 import * as constants from "./data";
@@ -6,12 +6,50 @@ import title from "./Poke Title.png";
 import "./GameMain.css";
 import GameOver from "./GameOver";
 
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+  apiKey: "AIzaSyBjVeVrmWYU1MNPJAAgcHM8jB3up5_nk3w",
+  authDomain: "pokeguesser-665bb.firebaseapp.com",
+  projectId: "pokeguesser-665bb",
+  storageBucket: "pokeguesser-665bb.appspot.com",
+  messagingSenderId: "4165695644",
+  appId: "1:4165695644:web:fd53578fc28f0a2256d559",
+  measurementId: "G-2K3N0HN11R",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 const GameMain = () => {
   const Phase = constants.Phase;
   const [options, setOptions] = useState(null);
   const [phase, setPhase] = useState(Phase.main);
   const [gens, setGens] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   const [stats, setStats] = useState({ correct: 0, score: 0 });
+
+  async function test() {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+  useEffect(() => {
+    test();
+  });
 
   const gameMode = () => {
     switch (phase) {
